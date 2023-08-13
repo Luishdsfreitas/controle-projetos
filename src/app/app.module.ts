4423f343271412e4e5fc7ca2dbe6f6041e113e03
Luis Freitas <luish.silveira@hotmail.com>
2023-08-13 20:49:21 -0300

    Add components Atividade-04

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { Teste1Component } from './teste1/teste1.component';
+import { Teste2Component } from './teste2/teste2.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Teste1Component,
+    Teste2Component
   ],
   imports: [
     BrowserModule,
