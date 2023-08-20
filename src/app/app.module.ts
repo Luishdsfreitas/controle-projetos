0605dcc2ea7ec760219c4834b43894a53d9358a2
Luis Freitas <luish.silveira@hotmail.com>
2023-08-20 19:34:53 -0300

    Add cabecalho e header como components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { RodapeComponent } from './rodape/rodape.component';
+import { CabecalhoComponent } from './cabecalho/cabecalho.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RodapeComponent,
+    CabecalhoComponent
   ],
   imports: [
     BrowserModule
