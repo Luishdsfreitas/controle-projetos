543ae0856d7c1d7ad64a3272634b8106ee6afc83
Luis Freitas <luish.silveira@hotmail.com>
2023-09-11 21:39:23 -0300

    Add funcionalidades atividades 13

--- src/app/detalhe-projeto/detalhe-projeto.component.ts
+++ src/app/detalhe-projeto/detalhe-projeto.component.ts
@@ -1,4 +1,7 @@
 import { Component, EventEmitter, Output } from '@angular/core';
+import { Projeto } from '../model/projeto';
+import { ActivatedRoute } from '@angular/router';
+import { ProjetoService } from '../services/projeto.service';
 
 @Component({
   selector: 'app-detalhe-projeto',
@@ -7,13 +10,41 @@ import { Component, EventEmitter, Output } from '@angular/core';
 })
 export class DetalheProjetoComponent {
 
-  @Output() eventoOutput: EventEmitter<string> = new EventEmitter<string>();
+  projeto:Projeto | undefined;
+  id: string | undefined;
 
+  constructor(private projetoService: ProjetoService, private route: ActivatedRoute){}
 
   ngOnInit() {
-    this.eventoOutput.emit("mensagem output event emitter");
+    this.route.params.subscribe(params => {
+      this.id = params['id'];
+      if (this.id) {
+        this.projetoService.buscarDetalhesProjetoPorId(this.id)
+          .subscribe((data) => {
+            this.projeto = data[0];
+          });
+      }
+    });
   }
 
-
-
 }
+
+
+
+  // constructor(private route: ActivatedRoute, private router: Router){}
+
+  // @Input() mensagem: string = '';
+
+  // mensagemOutput: string = '';
+
+  // numero: number | undefined;
+
+  // receberOutput(msg: string) {
+  //   this.mensagemOutput = msg;
+  // }
+
+  // ngOnInit(){
+  //   this.route.queryParams.subscribe((params) => {
+  //     this.numero = (params['numero'])
+  // });
+  // }
